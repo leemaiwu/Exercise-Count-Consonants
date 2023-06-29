c1b9a1ac14888b933afc5c1cf6927f879bc1568d
Laura Wu <leemaiwu@gmail.com>
2023-06-29 15:37:56 -0600

    Alternate solution with for loop

--- solution.js
+++ solution.js
@@ -5,3 +5,20 @@ const consonantCount = (str) => {
 
 console.log(consonantCount('bcdfghjklmnpqrstvwxyz')) // 21
 console.log(consonantCount('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ')) // 42
+
+// ---------------- Alternate Solution ----------------
+
+const consonantCounts = (str) => {
+    let consonant = 'bcdfghjklmnpqrstvwxyz'
+    let strArr = str.split('')
+    let count = 0
+    for (let i = 0; i < strArr.length; i++) {
+        if (consonant.includes(strArr[i].toLowerCase())) {
+            count++
+        }
+    }
+    return count
+}
+  
+  console.log(consonantCounts('bcdfghjklmnpqrstvwxyz')) // 21
+  console.log(consonantCounts('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ')) // 42
