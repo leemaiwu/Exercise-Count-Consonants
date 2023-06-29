e8ebb1fdace8d7caa05b779102489f2a6b48ab8e
Laura Wu <leemaiwu@gmail.com>
2023-06-29 15:31:10 -0600

    Completed with reduce and inclues method

--- solution.js
+++ solution.js
@@ -1,3 +1,7 @@
 const consonantCount = (str) => {
-  
+  let consonant = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'.split('')
+  return consonant.reduce((a, c) => a + str.includes(c), 0)
 }
+
+console.log(consonantCount('bcdfghjklmnpqrstvwxyz')) // 21
+console.log(consonantCount('bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ')) // 42
